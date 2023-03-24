0c3a135ad08953a5d9206da9776e417ee6c2e247
Sulaiman Ifeoluwa <ifeoluwasulaiman30@gmail.com>
2023-03-24 08:16:55 -0700

    done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# rThis manifest creates a file at /tmp
+file { '/tmp/school': 
+	ensure => 'file', 
+	owner => 'www-data', 
+        group => 'www-data', 
+	mode => '0744', 
+	content => 'I love 
+        Puppet',
+}

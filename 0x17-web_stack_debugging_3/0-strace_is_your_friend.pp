927fe3047ac5ab9c94d1ae681400fccec5313b31
Sulaiman Ifeoluwa <ifeoluwasulaiman30@gmail.com>
2023-05-10 19:37:10 +0000

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path => '/usr/local/bin/:/bin/'
+exec {'replacing_right path':
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif => 'test -f /var/www/html/wp-settings.php'
 }
